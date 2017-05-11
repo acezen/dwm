46c3645123080a32aa3756f5544c9be284b98891
acezen <weibin.zen@gmail.com>
2017-05-11 08:28:04 +0800

    folk from archlinux

--- config.h
+++ config.h
@@ -63,8 +63,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *fmcmd[] = { "thunar", NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *fmcmd[] = { "pcmanfm", NULL };
 static const char *wbcmd[] = { "firefox", NULL };
 static const char *fullscreenshot[] = { "scrot", "~/Pictures/screenshot/shot_%Y%m%d_%H.png" };
 static const char *partscreenshot[] = { "scrot", "-s", "~/Pictures/screenshot/shot_%Y%m%d_%H.png" };
